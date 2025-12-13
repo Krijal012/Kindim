d749f8e62b34650da4691be83186123657dd07f7
Pratyush Adhikari <pratyushadhikari785@gmail.com>
2025-12-13 12:32:43 +0545

    added header and the footer

---
+++ Kindim/src/components/header.jsx
@@ -0,0 +1,39 @@
+import logoIcon from "../assets/icons/logo-icon.png";
+
+function Header({ show = true }) {
+  return (
+    <header
+      className={`
+        flex items-center gap-4 px-6 py-3 bg-[#1A73E8]
+        fixed top-0 left-0 w-full z-[9999]
+        sm:px-8 sm:py-4 
+        md:px-10
+        transition-transform duration-300
+        ${show ? "translate-y-0" : "-translate-y-full"}
+      `}
+    >
+      <img
+        src={logoIcon}
+        alt="logo"
+        className="h-12 w-12 sm:h-14 sm:w-14 md:h-20 md:w-20"
+      />
+
+      <span className="text-white font-bold text-xl sm:text-2xl md:text-3xl">
+        Kindim
+      </span>
+
+      <form className="ml-10 flex flex-1 max-w-xl relative">
+        <i className="fas fa-search absolute left-4 top-1/2 -translate-y-1/2 text-gray-500"></i>
+        <input
+          type="text"
+          placeholder="Search products..."
+          className="w-full px-4 py-2 pl-12 rounded-md text-black outline-none"
+        />
+      </form>
+
+      <button className='ml-auto bg-[#3d87ff] text-black font-semibold px-4 py-2 rounded-md shadow hover:bg-gray-100 active:scale-95 transition'>Logout</button>
+    </header>
+  );
+}
+
+export default Header;
--- Kindim/src/pages/Profile.jsx
+++ Kindim/src/pages/Profile.jsx
@@ -1,19 +1,28 @@
 import Sidebar from "../components/profile/Sidebar";
 import PersonalInfo from "../components/profile/PersonalInfo";
-
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 export default function Profile() {
   return (
-    <div className="min-h-screen bg-white p-6">
-      <h1 className="text-2xl font-semibold mb-6">Profile</h1>
-
-      <div className="flex gap-6">
-             <Sidebar />
-          <PersonalInfo />
-
-     
+    <div className="h-screen flex flex-col">
       
+      <Header />
+
+      
+      <div className="flex-1 overflow-hidden">
+        <div className="h-full overflow-y-auto p-6">
+          <h1 className="text-2xl font-semibold mb-6">Profile</h1>
+
+          <div className="flex gap-6">
+            <Sidebar />
+            <PersonalInfo />
+          </div>
+        </div>
       </div>
+
+      
+      <Footer />
     </div>
   );
 }
