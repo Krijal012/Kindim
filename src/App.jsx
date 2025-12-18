9abacaa114939473ef1fa98893ab6fee0dce0ae8
subigya <subigyanepal743@gmail.com>
2025-12-18 21:16:16 +0545

    wall painting and candles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
-import Hoodie from './description/Hoodie';
+// import Hoodie from './description/Hoodie';
 
-import Tshirt from './description/Tshirt';  
-import Sneaker from './description/Sneaker';
-import Wallclock from './description/Wallclock';
-import Headphone from './description/Headphone';
+// import Tshirt from './description/Tshirt';  
+// import Sneaker from './description/Sneaker';
+// import Wallclock from './description/Wallclock';
+// import Headphone from './description/Headphone';
 // import CategoryPage from './pages/categorypage';
 // import ErrorPage from './errorpage';
 // // import ErrorPage from './pages/errorpage';
 // import Facewash from './description/Facewash';
 // import Hat from './description/Hat';
+import Candles from './description/Candles';
 
 
 
@@ -17,7 +18,7 @@ import Headphone from './description/Headphone';
 function App() {
   return (
     <>
-    <Headphone/>
+    <Candles/>
  
     </>
   );
