c937b58b7d80fa5f5773edaeb4501955f56da179
subigya <subigyanepal743@gmail.com>
2025-12-18 18:39:17 +0545

    added hat page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import './App.css'
 // import CategoryPage from './pages/categorypage';
 // import ErrorPage from './errorpage';
 // // import ErrorPage from './pages/errorpage';
-import Facewash from './description/Facewash';
+// import Facewash from './description/Facewash';
+import Hat from './description/Hat';
 
 
 
 function App() {
   return (
     <>
-    <Facewash/>
+    <Hat/>
  
     </>
   );
