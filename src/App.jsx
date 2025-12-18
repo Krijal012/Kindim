4d6330dcb0b8e2eaabc8850e9a2e1f3a01925902
subigya <subigyanepal743@gmail.com>
2025-12-18 18:58:40 +0545

    added tshirt

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
+import Tshirt from './description/Tshirt';
 // import CategoryPage from './pages/categorypage';
 // import ErrorPage from './errorpage';
 // // import ErrorPage from './pages/errorpage';
 // import Facewash from './description/Facewash';
-import Hat from './description/Hat';
+// import Hat from './description/Hat';
+
 
 
 
 function App() {
   return (
     <>
-    <Hat/>
+    <Tshirt/>
  
     </>
   );
