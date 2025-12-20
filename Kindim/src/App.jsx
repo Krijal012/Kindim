c58e9a412963800552c3faec19df574aecdfb4f5
panda-84 <bibekdhami225@gmail.com>
2025-12-20 20:39:22 +0545

    wishlist updated

--- Kindim/src/App.jsx
+++ Kindim/src/App.jsx
@@ -1,8 +1,13 @@
+import { CartItem } from "./components/WishCart";
 import DashBoard from "./pages/DashBoard";
+import Loginpage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import WishList from "./pages/WishList";
+
 
 export default function App() {
   // To see other pages, you can swap <DashBoard /> with:
   // import LoginPage from "./pages/LoginPage"; -> <LoginPage />
   // import SignupPage from "./pages/SignupPage"; -> <SignupPage />
-  return <DashBoard />;
+  return <WishList />;
 }
