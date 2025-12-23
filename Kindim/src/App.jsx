0d3ac05e8654adf863dbbd0cb3e27df315428eb8
krijal7@gmail.com <krijalshrestha7@gmail.com>
2025-12-23 22:29:38 +0545

    checked categorysection

--- Kindim/src/App.jsx
+++ Kindim/src/App.jsx
@@ -2,13 +2,14 @@
 import './App.css'
 import { Footer } from './Components/footer'
 import { Header } from './Components/header'
+import CategorySection from './pages/categorysection'
 import ErrorPage from './Pages/errorpage'
 
 function App() {
 
   return (
     <>
-      <ErrorPage />
+      <CategorySection />
     </>
   )
 }
