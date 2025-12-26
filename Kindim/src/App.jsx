6257b7999a976df92c9d239eded3c2aa12dadc82
krijal7@gmail.com <krijalshrestha7@gmail.com>
2025-12-26 10:23:35 +0545

    checking bargain page

--- Kindim/src/App.jsx
+++ Kindim/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import { Footer } from './Components/footer'
 import { Header } from './Components/header'
+import { BargainPage } from './pages/bargainpage'
 import { CartPage } from './Pages/cartpage'
 import CategorySection from './pages/categorysection'
 import ErrorPage from './Pages/errorpage'
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <>
-      <CategorySection />
+      <BargainPage />
     </>
   )
 }
