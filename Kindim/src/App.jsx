38e6547b03a81e54b588236748f16459a1496d86
krijal7@gmail.com <krijalshrestha7@gmail.com>
2026-01-03 15:03:31 +0545

    Initialize Kindim frontend with Vite + React project structure

---
+++ Kindim/src/App.jsx
@@ -0,0 +1,50 @@
+import './App.css'
+// import Hoodie from './description/Hoodie';
+
+// import Tshirt from './description/Tshirt';  
+// import Sneaker from './description/Sneaker';
+// import Wallclock from './description/Wallclock';
+// import Headphone from './description/Headphone';
+import CategoryPage from './pages/categorypage';
+// import ErrorPage from './errorpage';
+// // import ErrorPage from './pages/errorpage';
+// import Facewash from './description/Facewash';
+// import Hat from './description/Hat';
+// import Candles from './description/Candles';
+// import Jeans from './description/Jeans';
+// import Vase from './description/Vase';
+// import Indoorplant from './description/Indoorplant';
+// import Guitar from './description/Guitar';
+// import Microphone from './description/Microphone';
+// import Dj from './description/Dj';
+// import Drumpad from './description/Drumpad';
+// import Flute from './description/Flute';
+// import Piano from './description/Piano';
+// import Tv from './description/Tv';
+// import  Smartwatch from './description/Smartwatch';
+// import  Speaker from './description/Speaker';
+// import  Ps5 from './description/Ps5';
+// import Ukelele from './description/Ukelele';
+// import Laptop from './description/Laptop';
+// import Decorative from './description/Decorative';
+// import Tablelamp from './description/Tablelamp';
+// import Jacket from './description/Jacket';
+// import Faceclean from './description/faceclean';
+// import Denver from './description/Denver';
+// import Mamaearth from './description/Mamaearth';
+// import Poshperfumes from './description/Poshperfumes';
+import OrderHistorypage from './pages/Orderhistorypage';
+
+
+
+
+function App() {
+  return (
+    <>
+    <OrderHistorypage/>
+ 
+    </>
+  );
+}
+
+export default App;
