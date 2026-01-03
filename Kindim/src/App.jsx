805838a0f0dbfe62b08c0b222eb2f3fdd0f39935
Pratyush Adhikari <pratyushadhikari785@gmail.com>
2026-01-03 08:31:25 +0545

    -

--- Kindim/src/App.jsx
+++ Kindim/src/App.jsx
@@ -1,7 +1,15 @@
-
-import Checkout from "./pages/Checkout";
-
+import RewardDashboard from "./pages/RewardDashboard";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
-  return <Checkout/>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        <RewardDashboard />
+      </main>
+      <Footer />
+    </div>
+  );
 }
