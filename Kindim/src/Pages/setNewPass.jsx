7733c734249261c14cbece65589ab23729d4a9c9
prajinpokhrel <prajinpokhrel@gmail.com>
2026-01-03 13:48:24 +0545

    checking code error

--- Kindim/src/Pages/setNewPass.jsx
+++ Kindim/src/Pages/setNewPass.jsx
@@ -1,13 +1,19 @@
 import AuthBox from "../Components/AuthBox";
 import Left from "../Components/left";
 import Right from "../Components/right";
+import Header from "../Components/header";
+import Footer from "../Components/footer";
 
 function SetNewPass() {
     return (
-        <AuthBox>
-            <Left />
-            <Right />
-        </AuthBox>
+        <>
+            <Header />
+            <AuthBox>
+                <Left />
+                <Right />
+            </AuthBox>
+            <Footer />
+        </>
     );
 }
 
