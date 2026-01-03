313cc1090919d5de1fa5a55ca00aef6d6ff1aa33
Pratyush Adhikari <pratyushadhikari785@gmail.com>
2026-01-03 08:23:39 +0545

    -

---
+++ Kindim/src/pages/RewardDashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import wheelImg from "../assets/icons/wheel.png";
+
+export default function RewardDashboard() {
+  return (
+    <div className="min-h-screen bg-white flex justify-center p-5">
+      <div className="w-full max-w-5xl border-[3px] border-blue-500 p-5">
+        
+    
+        <h2 className="text-center text-xl font-bold mb-5">
+          Your Reward Dashboard
+        </h2>
+
+       
