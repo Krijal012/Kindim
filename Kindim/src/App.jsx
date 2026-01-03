5d931418036fdcde4309d97056020aa163eaca99
prajinpokhrel <prajinpokhrel@gmail.com>
2026-01-03 13:47:56 +0545

    checking each pages individually

--- Kindim/src/App.jsx
+++ Kindim/src/App.jsx
@@ -1,15 +1,13 @@
 import "./App.css";
-import AdminDashboard from "./Pages/admindashboard";
-import SellerDashboard from "./Pages/sellerdashboard";
-import UserManagement from "./Pages/usermanagement";
-//  import OrderManagement from "./Components/ordermanagement";
-//  import UserManagement from "./Components/usermanagement";
-// import AdminDashboard from "./Components/admindashboard";
-// import UserDashboard from "./Components/UserDashboard";
-// import SetNewPass from "./Pages/SetNewPass";
+import { AdminDashboard } from "./Pages/admindashboard";
+import { MyOrder } from "./Pages/myorders";
+import { MyProducts } from "./Pages/myproducts";
+import { SellerDashboard } from "./Pages/sellerdashboard";
+import { SellerManagement } from "./Pages/sellermanagement";
+import { UserManagement } from "./Pages/usermanagement";
 
 function App() {
-  return <SellerDashboard/>;
+  return <UserManagement />;
 }
 
 export default App;
